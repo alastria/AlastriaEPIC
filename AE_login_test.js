b64877e6a56003d57cd70648bce480973f53158a
Juan Francisco Tavira Manjon <jftavira@gmail.com>
2022-08-19 16:02:25 +0200

    new user_wallet, new entity_wallet both extend root_wallet. Credential Issuance progressed

--- AE_login_test.js
+++ AE_login_test.js
@@ -9,9 +9,8 @@ async function main() {
 
 
     console.log ("1st test: create HDWallets");
-
-    let newUserEpicWallet = new AEW.AE_wallet();
-    let newEntityEpicWallet = new AEW.AE_wallet();
+    let newUserEpicWallet = new AEW.AE_userWallet();
+    let newEntityEpicWallet = new AEW.AE_entityWallet();
 
     //const mnemonic = bip39.generateMnemonic();
     newUserEpicWallet.setMnemonic("used rebel ahead harvest journey steak hub core opera wrong rate loan");
@@ -31,13 +30,16 @@ async function main() {
     console.log ("\t3rd test, 1st step: Setup communication wallets and keys");
 
 
-    // in order to login with AcmeAcademy the user will create a new derivation por AcmeAcademy, exteding Z0_A0_A with a random derivation for AcmeAcademy and remembering / storing it
+    // in order to login with AcmeAcademy the user will create a new derivation por AcmeAcademy, 
+    // exteding Z0_A0_A with a random derivation for AcmeAcademy and remembering / storing it
     // AcmeAcademy will be 6385471, random number just for this user
     // the complete derivation of AcmeAcademy for the user would be: "m/1037171/131071/0407/10011001/94367/3651441/6385471"
     newUserEpicWallet.addBPlusDerivation("AcmeAcademy","6385471");
     
-    // when connecting with AcmeAcademy the user will tell AcmeAcademy his private key for the communications with AcmeAcademy
-    // there are two modes of creting this wallet: either directly from the testing_wallet or creating a derivation, we will choose the second    
+    // when connecting with AcmeAcademy the user will tell AcmeAcademy his public key for the communications with AcmeAcademy
+    // there are two modes of creting this wallet: 
+    // 1. From the base_wallet with the whole derivation
+    // 2. From the identity_wallet using just the B+ derivation for AcmeAcademy, we will choose this one
     AcmeAcademy = newUserEpicWallet.getBPlusDerivation("AcmeAcademy");
     user_acme_relationship_wallet = AEL.getHDWalletDerivation(newUserEpicWallet.identity_HDWallet , "m/" + AcmeAcademy.B_derivation);
     // I tell AcmeAcademy my user_acme_relationship_public_key, that is equivalent to my DID only for AcmeAcademy
@@ -56,24 +58,24 @@ async function main() {
     
     // AcmeAcademy also determines a random derivation for its communications with the user
     // User will be 241573, random number just for this AcmeAcademy
-    newEntityEpicWallet.addBPlusDerivation("User","241573");
-    user = newEntityEpicWallet.getBPlusDerivation("User");
+    newEntityEpicWallet.addCPlusDerivation("User","241573");
+    user = newEntityEpicWallet.getCPlusDerivation("User");
 
-    // AcmeAcademy creates a wallet for its relationship with the user <---------------
-    acme_user_relationship_wallet = AEL.getHDWalletDerivation(newEntityEpicWallet.identity_HDWallet, "m/"+user.B_derivation);
+    // AcmeAcademy creates a wallet for its relationship with the user <--------------- THINK!!!
+    acme_user_relationship_wallet = AEL.getHDWalletDerivation(newEntityEpicWallet.identity_HDWallet, "m/"+user.C_derivation);
     acme_user_relationship_public_key = AEL.getPublicExtendedKey(acme_user_relationship_wallet);
 
 
     // Update wallets with exchanged publicKeys    
     newUserEpicWallet.updateBPlusDerivationExtendedKeys("AcmeAcademy",acme_user_relationship_public_key,user_acme_relationship_public_key)
-    newEntityEpicWallet.updateBPlusDerivationExtendedKeys("User",user_acme_relationship_public_key,acme_user_relationship_public_key)
+    newEntityEpicWallet.updateCPlusDerivationExtendedKeys("User",user_acme_relationship_public_key,acme_user_relationship_public_key)
 
 
 
     console.log ("\t3rd test, 2nd step: Login challenge");
-    // acme sends me a login challenge
+    // acme sends me a login challenge, adding its Extended Public Key acting as DID
     var acme_login_challenge = "{'message':'please sign with your Public Key to login','my_publicKey':'replace'}";
-    acme_login_challenge = acme_login_challenge.replace("replace", newEntityEpicWallet.getBPlusDerivation("User").own_extendedPublicKey);
+    acme_login_challenge = acme_login_challenge.replace("replace", AEL.getPublicExtendedKey(newEntityEpicWallet.login_HDWallet));
 
     // User will create an HDWallet for his communications with ACME Academy
     // common knowledge: "/0" will be the standar derivation for "login" for the user
@@ -91,10 +93,10 @@ async function main() {
         );
 
     // User signs login challenge
-    let acme_login_challenge_signed = await AEL.signMessage(user_acme_login_signer_eWallet, acme_login_challenge);    
+    let acme_login_challenge_signature = await AEL.signMessage(user_acme_login_signer_eWallet, acme_login_challenge);    
 
     // AcmeAcademy verifies signature with the original challenge and the extendedPublicKey AcmeAcademy calculated from the User PubK + Derivation <------
-    AEL.verifyLoginMessage(acme_login_challenge,acme_login_challenge_signed,AcmeAcademy.other_extendedPublicKey);
+    AEL.verifyLoginMessage(acme_login_challenge,acme_login_challenge_signature,AcmeAcademy.other_extendedPublicKey);
 
     }
 
--- AE_wallet.js
+++ AE_wallet.js
@@ -9,8 +9,7 @@ const userEpicWallet = {
     Bplus_derivation: []
 };
 
-
-class AE_wallet {
+class AE_rootWallet {
     constructor() {
         this.mnemonic = "",
         this.base_HDWallet = "",
@@ -27,6 +26,13 @@ class AE_wallet {
         this.identity_derivation = identityDerivationStr;
         this.identity_HDWallet = AE.getHDWalletDerivation(this.base_HDWallet, this.identity_derivation);
     }
+}
+
+class AE_userWallet extends AE_rootWallet{
+    constructor() {
+        super();
+
+    }
     addBPlusDerivation (entityStr, derivationStr) {
         let localBPD = {};
         localBPD.entity = entityStr;
@@ -39,12 +45,56 @@ class AE_wallet {
     updateBPlusDerivationExtendedKeys (entityStr, ownExtendedKey, other_extendedKey) {
         let localBplus = this.getBPlusDerivation(entityStr);
         let localBplusIdx = this.Bplus_derivation.findIndex(element => element.entity === entityStr);
-        localBplus.own_extendedPublicKey = acme_user_relationship_public_key;
-        localBplus.other_extendedPublicKey = user_acme_relationship_public_key;
+        localBplus.own_extendedPublicKey = ownExtendedKey;
+        localBplus.other_extendedPublicKey = other_extendedKey;
         this.Bplus_derivation[localBplusIdx] = localBplus;
     }
 }
 
+class AE_entityWallet extends AE_rootWallet{
+    constructor() {
+        super();        
+        this.Cplus_derivation = [];
+    }
+    
+    setMnemonic (mnemonicStr) {
+        super.setMnemonic(mnemonicStr);
+    }
+
+    setIdentityDerivation (identityDerivationStr) {
+        super.setIdentityDerivation(identityDerivationStr);
+        // This corresponds to C derivations aka Purpose
+        // 0 -> login, may be usefull for C2C interactions or to sign login challenges
+        this.login_derivation = "m/0";        
+        this.login_HDWallet = AE.getHDWalletDerivation(this.base_HDWallet, this.login_derivation);
+        // 1 -> credencial issuance
+        this.credencialIssuance_derivation = "m/1";
+        this.credencialIssuance_HDWallet = AE.getHDWalletDerivation(this.base_HDWallet, this.credencialIssuance_derivation);
+        // 2 -> presentations
+        this.presentations_derivation = "m/2";
+        this.presentations_HDWallet = AE.getHDWalletDerivation(this.base_HDWallet, this.presentations_derivation);
+
+    }
+
+    addCPlusDerivation (entityStr, derivationStr) {
+        let localCPD = {};
+        localCPD.entity = entityStr;
+        localCPD.C_derivation = derivationStr;
+        this.Cplus_derivation.push(localCPD);
+    }
+    getCPlusDerivation (entityStr) {        
+        return this.Cplus_derivation.find(element => element.entity === entityStr);;
+    }
+    updateCPlusDerivationExtendedKeys (entityStr, ownExtendedKey, other_extendedKey) {
+        let localCplus = this.getCPlusDerivation(entityStr);
+        let localCplusIdx = this.Cplus_derivation.findIndex(element => element.entity === entityStr);
+        localCplus.own_extendedPublicKey = ownExtendedKey;
+        localCplus.other_extendedPublicKey = other_extendedKey;
+        this.Cplus_derivation[localCplusIdx] = localCplus;
+    }
+}
+
 module.exports = {
-    AE_wallet: AE_wallet
+    AE_userWallet: AE_userWallet,
+    AE_entityWallet: AE_entityWallet
 }
